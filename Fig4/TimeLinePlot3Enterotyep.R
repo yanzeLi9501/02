53568bcd05c6f4c4572c10679a8d39d4e37a2249
yanzeLi9501 <39907109+yanzeLi9501@users.noreply.github.com>
2019-12-09 15:46:19 +0800

    Update TimeLinePlot3Enterotyep.R

--- Fig4/TimeLinePlot3Enterotyep.R
+++ Fig4/TimeLinePlot3Enterotyep.R
@@ -7,8 +7,7 @@
 BT<-read.csv("TimeLine6SplitEnterotype.csv")
 library(ggplot2)
  # turn-off scientific notation like 1e+48
-human<-BT[c(1:6),]
-mouse<-BT[c(7:12),]
+
 gg<-ggplot(data=BT,aes(x=days,y=changeratio,group=enterotype,colour=enterotype))+
   theme(axis.text.x = element_text(angle=90, vjust=0.6)) +
   theme_classic() +
